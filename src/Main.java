e815c98013a5c98ce05d5e99aaf4a0a75d7887dc
Nil Viñals Nin <nil.vinals@col.vueling.com>
2023-04-07 13:22:48 +0200

    Functional first menu

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,80 @@
 import java.util.Scanner;
-
 import utils.Utilities;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("・・・・・・・・・・・・・・❥✿BIENVENIDE ❥✿・・・・・・・・・・・・・・");
-        int resultado = Utilities.llegirInt("Elige una de las siguientes opciones: \n 1. Log in cliente \n 2. Log in empresa \n 3. Log in vendedor", 1, 5);
-
-            switch (resultado) {
-                case 1:
-                    System.out.println("Menú cliente");
-                    break;
-                case 2:
-                    System.out.println("Menú empresa");
-                    break;
-                case 3:
-                    System.out.println("Menú vendedor");
-                    break;
-                case 4:
-                    System.out.println("Saliendo del programa...");
-                    break;
-                default:
-                    System.out.println("Opción inválida. Intente nuevamente.");
-                    break;
-            }
+        LoginMenu(scanner);
     }
+
+    private static void LoginMenu(Scanner scanner){
+        int selection = Integer.MAX_VALUE;
+
+        while(selection != 1){
+            System.out.println("・・・・・・・・・・・・・・❥✿ BIENVENIDE ❥✿・・・・・・・・・・・・・・");
+            selection = Utilities.llegirInt("Elige una de las siguientes opciones: \n 1. Salir de la aplicación \n 2. Log In \n 3. Sign Up", 1, 3);
+            switch (selection){
+                case 1: System.out.println("Saliendo de la aplicación...");
+                    break;
+
+                case 2: System.out.println("Log In");
+                    String email = Utilities.llegirParaula("Introduce tu email: ");
+                    String password = Utilities.llegirParaula("Introduce tu contraseña: ");
+                    //TODO: COMPROVAR A TRAVÉS DE RESEX QUE ES UN MAIL
+                    //TODO: COMPROVAR EXISTENCIA D'USUARI SEGONS ELS PARAMETRES PASSATS....
+                    IndividualMenu(scanner);
+                    break;
+
+                case 3: System.out.println("Sign Up");
+                    break;
+
+                default: selection = Integer.MAX_VALUE; break;
+            }
+        }
+    }
+
+    private static void IndividualMenu(Scanner scanner){
+        int selection = Integer.MAX_VALUE;
+
+        while(selection != 1){
+            selection = Utilities.llegirInt("Elige una de las siguientes opciones: \n 1. Salir de la sesion \n 2. Listar productos \n 3. Comprar producto \n 4. Modificar usuario", 1, 5);
+            switch (selection){
+                case 1: System.out.println("Saliendo de la sesión..."); break;
+                case 2: System.out.println("Listando productos"); break;
+                case 3: System.out.println("Comprando producto"); break;
+                case 4: System.out.println("Modificando usuario"); break;
+            }
+        }
+    }
+
+    private static void BusinessMenu(Scanner scanner){
+        int selection = Integer.MAX_VALUE;
+
+        while(selection != 1){
+            System.out.println("・・・・・・・・・・・・・・❥✿ BIENVENIDE ❥✿・・・・・・・・・・・・・・");
+            selection = Utilities.llegirInt("Elige una de las siguientes opciones: \n 1. Log in cliente \n 2. Log in empresa \n 3. Log in vendedor", 1, 5);
+            switch (selection){
+                case 1: System.out.println("Saliendo de la aplicación..."); break;
+                case 2: System.out.println("Log In"); break;
+                case 3: System.out.println("Sign Up"); break;
+            }
+        }
+    }
+
+    private static void SupplierMenu(Scanner scanner){
+        int selection = Integer.MAX_VALUE;
+
+        while(selection != 1){
+            System.out.println("・・・・・・・・・・・・・・❥✿ BIENVENIDE ❥✿・・・・・・・・・・・・・・");
+            selection = Utilities.llegirInt("Elige una de las siguientes opciones: \n 1. Log in cliente \n 2. Log in empresa \n 3. Log in vendedor", 1, 5);
+            switch (selection){
+                case 1: System.out.println("Saliendo de la aplicación..."); break;
+                case 2: System.out.println("Log In"); break;
+                case 3: System.out.println("Sign Up"); break;
+            }
+        }
+    }
+
+
 }
--- src/models/Order.java
+++ src/models/Order.java
@@ -3,15 +3,16 @@ package models;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Order {
     private int orderId;
     private ArrayList<GenericProduct> products;
-    private LocalDateTime orderCreationDate;
-    private LocalDate orderArrivalDate;
+    private Date orderCreationDate;
+    private Date orderArrivalDate;
 
-    public Order(int orderId, ArrayList<GenericProduct> products, LocalDateTime orderCreationDate,
-            LocalDateTime orderArrivalDate) {
+    public Order(int orderId, ArrayList<GenericProduct> products, Date orderCreationDate,
+                 Date orderArrivalDate) {
         this.orderId = orderId;
         this.products = products;
         this.orderCreationDate = orderCreationDate;
